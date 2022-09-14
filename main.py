634ff5c236f0113e02de123fb355432c9020008d
15565681561 <74585605+15565681561@users.noreply.github.com>
2022-09-14 16:25:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,11 +47,11 @@ def get_weather():
 # 获取情话
 def get_text():
   # url = "https://v2.alapi.cn/api/qinghua?token=LwExDtUWhF3rH5ib"
-  url = "https://api.vvhan.com/api/love"
-  res = requests.get(url)
+  url = "https://api.vvhan.com/api/love?type=json"
+  res = requests.get(url).json()
   if res is None:
     return None
-  text = res['text']
+  text = res['ishan']
   return text
 
 # 获取当前日期为星期几
